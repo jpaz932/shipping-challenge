44296bd3881133e2adf2ed1bc14ffaf318a130e3
jpaz932 <juanpa.aristis@gmail.com>
2025-02-08 02:24:56 -0500

    implement authentication routes with DTO validation middleware

--- src/auth/infrastructure/rest/authRoutes.ts
+++ src/auth/infrastructure/rest/authRoutes.ts
@@ -1,13 +1,12 @@
 import { FastifyInstance } from "fastify";
+import { AuthController } from "./authController";
+import { validateDto } from "@src/common/middlewares/validateDto";
+import { RegisterUserDto } from "@src/auth/domain/dtos/register.dto";
 
+const controller = new AuthController();
 export const authRoutes = (fastify: FastifyInstance, opts: any, done: () => void) => {
-    fastify.post('/login', async (request, reply) => {
-        reply.send({ message: 'Login' });
-    });
-    
-    fastify.post('/register', async (request, reply) => {
-        reply.send({ message: 'Register' });
-    });
+    fastify.post('/login', controller.loginUser);
+    fastify.post('/register', { preHandler: validateDto(RegisterUserDto) }, controller.registerUser);
 
     done();
 }
---
+++ src/common/middlewares/validateDto.ts
@@ -0,0 +1,26 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { ClassConstructor } from 'class-transformer/types/interfaces';
+
+
+export const validateDto = (dto: ClassConstructor<any>) => {
+    return async (request: FastifyRequest, reply: FastifyReply, next: (err?: Error) => void) => {
+        const dtoInstance = plainToClass(dto, request.body);
+        const errors = await validate(dtoInstance);
+        console.log(errors);
+        if (errors.length > 0) {
+            const errorResponse = {
+                message: 'Validation error',
+                errors: errors.map((error) => ({
+                    field: error.property,
+                    messages: error.constraints ? Object.values(error.constraints) : [],
+                })),
+            };
+            
+            return reply.status(400).send(errorResponse);
+        }
+
+        next();
+    };
+};
