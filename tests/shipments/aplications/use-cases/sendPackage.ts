ce0ab3c83969ebcb71e21b546a868847b835c573
jpaz932 <juanpa.aristis@gmail.com>
2025-02-10 00:33:24 -0500

    add sendPackage schema and tests; implement SendPackage use case and mock repository

---
+++ tests/shipments/aplications/use-cases/sendPackage.ts
@@ -0,0 +1,22 @@
+import { ShipmentRepository } from 'tests/shipments/infrastructure/__mocks__/ShipmentRepository';
+import { SendPackage } from '@src/shipments/application/use-cases/sendPackage';
+
+describe('SendPackage use case', () => {
+    it('should return a shipment when the user creates it', async () => {
+        const repository = new ShipmentRepository();
+        const registerUseCase = new SendPackage(repository);
+
+        const user = await registerUseCase.execute({
+            phone: 123456789,
+            address: 'Calle 123',
+            dimensions: '10x10x10',
+            product_type: 'Electronics',
+            weight: 10,
+            origin_city: 'Bogota',
+            destination_city: 'Medellin',
+            userId: 1,
+        });
+
+        expect(user).toHaveProperty('token');
+    });
+});
---
+++ tests/shipments/infrastructure/__mocks__/ShipmentRepository.ts
@@ -0,0 +1,25 @@
+import { Shipment } from '@src/shipments/domain/entities/shipment.entity';
+import { ShipmentDto } from '@src/shipments/domain/dto/shipment.dto';
+
+export class ShipmentRepository {
+    private shipments: Shipment[] = [];
+
+    async sendPackage(shipmentDto: ShipmentDto): Promise<Shipment> {
+        const shipment = new Shipment(
+            this.shipments.length + 1,
+            1,
+            shipmentDto.phone,
+            shipmentDto.address,
+            shipmentDto.dimensions,
+            shipmentDto.product_type,
+            shipmentDto.weight,
+            shipmentDto.origin_city,
+            shipmentDto.destination_city,
+            'Pendiente',
+            new Date(),
+            new Date(),
+        );
+        this.shipments.push(shipment);
+        return Promise.resolve(shipment);
+    }
+}
