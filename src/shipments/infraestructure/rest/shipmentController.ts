cacb836398fce3ad95bae34a954cb92d43cd849c
jpaz932 <juanpa.aristis@gmail.com>
2025-02-24 15:40:59 -0500

    refactor: update ShipmentController to use use-case classes for better separation of concerns

--- src/shipments/infraestructure/rest/shipmentController.ts
+++ src/shipments/infraestructure/rest/shipmentController.ts
@@ -6,6 +6,11 @@ import { SendPackage } from '@src/shipments/application/use-cases/sendPackage';
 import { GetAllShipments } from '@src/shipments/application/use-cases/getAllShipments';
 import { CustomError } from '@src/common/errors/custom.error';
 import { StatusRouteDto } from '@src/shipments/domain/dto/routeDto';
+import { GetAllCarriers } from '@src/shipments/application/use-cases/getAllCarriers';
+import { GetShipmentByTrackingCode } from '@src/shipments/application/use-cases/getShipmentByTrackingCode';
+import { AssignShipmentToCarrier } from '@src/shipments/application/use-cases/asignShipmentToCarrier';
+import { GetAllRoutes } from '@src/shipments/application/use-cases/getAllRoutes';
+import { ChangeRouteStatus } from '@src/shipments/application/use-cases/changeRouteStatus';
 
 export class ShipmentController {
     constructor(private readonly shipmentRepository: ShipmentRepository) {}
@@ -27,8 +32,8 @@ export class ShipmentController {
     };
 
     getAllCarriers = (request: FastifyRequest, reply: FastifyReply) => {
-        this.shipmentRepository
-            .getAllCarriers()
+        new GetAllCarriers(this.shipmentRepository)
+            .execute()
             .then((response) => reply.status(200).send(response))
             .catch((error) => handlerError(error, reply));
     };
@@ -46,8 +51,8 @@ export class ShipmentController {
             );
         }
 
-        this.shipmentRepository
-            .assignShipmentToCarrier(shipmentId)
+        new AssignShipmentToCarrier(this.shipmentRepository)
+            .execute(shipmentId)
             .then((response) => reply.status(200).send(response))
             .catch((error) => handlerError(error, reply));
     };
@@ -65,15 +70,15 @@ export class ShipmentController {
             );
         }
 
-        this.shipmentRepository
-            .getShipmentByTrackingCode(trackingCode)
+        new GetShipmentByTrackingCode(this.shipmentRepository)
+            .execute(trackingCode)
             .then((response) => reply.status(200).send(response))
             .catch((error) => handlerError(error, reply));
     };
 
     getAllRoutes = (request: FastifyRequest, reply: FastifyReply) => {
-        this.shipmentRepository
-            .getAllRoutes()
+        new GetAllRoutes(this.shipmentRepository)
+            .execute()
             .then((response) => reply.status(200).send(response))
             .catch((error) => handlerError(error, reply));
     };
@@ -81,8 +86,8 @@ export class ShipmentController {
     changeRouteStatus = (request: FastifyRequest, reply: FastifyReply) => {
         const statusRouteDto = request.body as StatusRouteDto;
 
-        this.shipmentRepository
-            .changeRouteStatus(statusRouteDto)
+        new ChangeRouteStatus(this.shipmentRepository)
+            .execute(statusRouteDto)
             .then((response) => reply.status(200).send(response))
             .catch((error) => handlerError(error, reply));
     };
